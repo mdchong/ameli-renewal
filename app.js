3c75228ce5339b6de1e213619cfc2859470a6cc5
mdchong <mdchong@asu.edu>
2025-06-26 03:36:28 +0900

    .

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let currentIndex = 0;
+const items = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+    items.forEach((item, i) => {
+        item.classList.remove('active');
+    })
+    items[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % items.length;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + items.length) % items.length;
+    showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
